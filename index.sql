3aa0fb45a7952771f7540fa890420262da48cbec
Eliud <omegaeliud1111@gmail.com>
2025-05-02 23:56:24 -0600

    Modified index.sql with better indexes; indexLog logs before and after times for running query.log; queryLOGS.txt updated after inserting new Sales in relation in the Sales.sql

--- index.sql
+++ index.sql
@@ -1,4 +1,5 @@
 CREATE INDEX desc_index ON Inventory(description); 
-CREATE INDEX upc_Inv_index ON Inventory(upc); 
+
 CREATE INDEX vendorId_Inv_index ON Inventory(vendor_id); 
-CREATE INDEX vendorId_Vend_index ON Vendor(description);
+CREATE INDEX price_Inv_index ON Inventory(price);
+CREATE INDEX qty_Inv_Index ON Inventory(quantity);
