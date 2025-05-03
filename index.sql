ed3935cb137f5faaa198fe2cf1402b5fe704ef5c
SpicyTuna <132015252+seriousSalamander@users.noreply.github.com>
2025-05-02 22:21:36 -0600

    added  indexes.

---
+++ index.sql
@@ -0,0 +1,4 @@
+CREATE INDEX desc_index ON Inventory(description); 
+CREATE INDEX upc_Inv_index ON Inventory(upc); 
+CREATE INDEX vendorId_Inv_index ON Inventory(vendor_id); 
+CREATE INDEX vendorId_Vend_index ON Vendor(description);
